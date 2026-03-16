ab30831f2ef12abe517ca0c7c1177005b4be5c7f
Samuel Adeogun <sadeogun007@gmail.com>
2026-03-16 12:52:39 +0000

    sagq3

--- session-1/task2/points.c
+++ session-1/task2/points.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include "points.h"
+#include <math.h>
 
 int main( void ) {
 
@@ -14,10 +15,15 @@ int main( void ) {
     printf(" Distance between (%.1f,%.1f) and (%.1f,%.1f) is %f\n", 
              p1.x,p1.y,p2.x,p2.y,distance(p1,p2));
     */
+    Point p1 = { .x=1, .y=2 };
+    Point p2 = { .x=-2, .y=4 };
+
+    printf(" Distance between (%.1f,%.1f) and (%.1f,%.1f) is %f\n", 
+             p1.x,p1.y,p2.x,p2.y,distance(p1,p2));
     return 0;
 }
 
 float distance( Point p, Point q ) {
-    // implement distance here
-    return 0.0;
+    float distance = sqrt((pow((q.x - p.x), 2)) + (pow((q.y - p.y), 2)));
+    return distance;
 }
